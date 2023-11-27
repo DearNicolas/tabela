0f4d4ff14c0dd87a73605a43002286f388067910
bonnye <bonnye@gmal.com>
2023-11-27 16:51:32 -0300

    atualisaçao2

--- src/models/itens.ts
+++ src/models/itens.ts
@@ -1,55 +1,55 @@
 let data = [
     {
-           "Total": "42",
-           "Mes": "1",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "22",
-           "Mes": "2",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "25",
-           "Mes": "3",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "25",
-           "Mes": "4",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "20",
-           "Mes": "5",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "26",
-            "Mes": "6",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "27",
-           "Mes":"7",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "27",
-           "Mes": "8",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "36",
-           "Mes": "9",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       },
-       {
-           "Total": "27",
-           "Mes": "10",
-           "Cidade": "\"AFONSO CLÁUDIO\""
-       }
-   ];
+        "Total": "42",
+        "Mes": "1",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "22",
+        "Mes": "2",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "25",
+        "Mes": "3",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "25",
+        "Mes": "4",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "20",
+        "Mes": "5",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "26",
+        "Mes": "6",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "27",
+        "Mes":"7",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "27",
+        "Mes": "8",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "36",
+        "Mes": "9",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    },
+    {
+        "Total": "27",
+        "Mes": "10",
+        "Cidade": "\"AFONSO CLÁUDIO\""
+    }
+];
 export const tabela = {
     getAll:() => {
         return data;
