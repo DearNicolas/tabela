220036b31edba77f7d744fa8cfdf8e574583edd9
nicolas <nicolas.dearmini@gmail.com>
2023-11-27 15:42:47 -0300

    projeto do samuca atualizaçao3

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -4,13 +4,8 @@ import {tabela,pegarNomeMes} from '../models/itens'
 
 export const home = (req: Request, res:Response) =>{
     let list = tabela.getAll();
-    let numeroNome = pegarNomeMes;
     res.render('pages/page',{
-        banner: {
-            title: 'Tabela',
-            background: 'AfonsoClaudio.jpg'
-        },
         list,
-        numeroNome
+        
     });
 };
